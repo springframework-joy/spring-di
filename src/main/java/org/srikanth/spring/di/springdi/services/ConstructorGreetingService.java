ad57c14baf9ea2f63c01d8b04f019bb1ebc56453
springframework-joy <srikanth.josyula@gmail.com>
2018-09-03 13:33:10 -0400

    Added Bean Life Cycle

--- src/main/java/org/srikanth/spring/di/springdi/services/ConstructorGreetingService.java
+++ src/main/java/org/srikanth/spring/di/springdi/services/ConstructorGreetingService.java
@@ -2,8 +2,15 @@ package org.srikanth.spring.di.springdi.services;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Service
 public class ConstructorGreetingService implements GreetingService{
+
+
+
+
     @Override
     public String sayGreeting() {
         return "Hello - I was injected via the Constructor!!";
--- src/main/java/org/srikanth/spring/di/springdi/services/GetterGreetingService.java
+++ src/main/java/org/srikanth/spring/di/springdi/services/GetterGreetingService.java
@@ -1,12 +1,16 @@
 package org.srikanth.spring.di.springdi.services;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GetterGreetingService implements GreetingService{
+public class GetterGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
         return "Hello - I was injected via the Getter!!";
     }
+
+
 }
