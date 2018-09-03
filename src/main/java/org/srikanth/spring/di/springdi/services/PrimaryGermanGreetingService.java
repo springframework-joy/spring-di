fe08345669b365b7388a3fac4385452c0a6eb79b
springframework-joy <srikanth.josyula@gmail.com>
2018-09-03 12:36:43 -0400

    added Profiles
    @Profile({"en", "default"})

--- src/main/java/org/srikanth/spring/di/springdi/services/PrimaryGermanGreetingService.java
+++ src/main/java/org/srikanth/spring/di/springdi/services/PrimaryGermanGreetingService.java
@@ -1,16 +1,15 @@
 package org.srikanth.spring.di.springdi.services;
 
-
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("de")
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello!!! Primary Greeting Service";
+        return "Prmarer GuBdienst";
     }
 }
