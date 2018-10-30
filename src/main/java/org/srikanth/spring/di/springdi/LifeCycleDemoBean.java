4e0a9fde7920e4e3043d047cd1067a1837dc14fa
N000466 <N000466@XD-DEV-JS.corp.odfl.com>
2018-10-30 16:36:11 -0400

    test

--- src/main/java/org/srikanth/spring/di/springdi/LifeCycleDemoBean.java
+++ src/main/java/org/srikanth/spring/di/springdi/LifeCycleDemoBean.java
@@ -1,65 +1,68 @@
 package org.srikanth.spring.di.springdi;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
-import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 @Component
 public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("setBeanFactory invoked");
-    }
+   @Override
+   public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+      System.out.println("setBeanFactory invoked");
+      System.out.println("Test");
+   }
 
-    @Override
-    public void setBeanName(String s) {
-        System.out.println("setBeanName invoked");
-    }
+   @Override
+   public void setBeanName(String s) {
+      System.out.println("setBeanName invoked");
+   }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("destroy invoked");
+   @Override
+   public void destroy() throws Exception {
+      System.out.println("destroy invoked");
 
-    }
+   }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet invoked");
+   @Override
+   public void afterPropertiesSet() throws Exception {
+      System.out.println("afterPropertiesSet invoked");
 
-    }
+   }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("setApplicationContext invoked");
+   @Override
+   public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+      System.out.println("setApplicationContext invoked");
 
-    }
+   }
 
-    @PreDestroy
-    public void preDestroy(){
-        System.out.println("preDestroy invoked");
+   @PreDestroy
+   public void preDestroy() {
+      System.out.println("preDestroy invoked");
 
-    }
+   }
 
-    @PostConstruct
-    public void postConstruct(){
-        System.out.println("postConstruct invoked");
+   @PostConstruct
+   public void postConstruct() {
+      System.out.println("postConstruct invoked");
 
-    }
+   }
 
-    public void beforeInit(){
-        System.out.println("beforeInit invoked");
+   public void beforeInit() {
+      System.out.println("beforeInit invoked");
 
-    }
+   }
 
-    public void afterInit(){
-        System.out.println("afterInit invoked");
+   public void afterInit() {
+      System.out.println("afterInit invoked");
 
-    }
+   }
 }
-
